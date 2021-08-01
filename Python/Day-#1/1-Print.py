3b833429c355379ff3c9f107542adf10fa056956
nkrishnappa <nandisha111@gmail.com>
2021-08-01 20:40:45 +0530

    Day #1

---
+++ Python/Day-#1/1-Print.py
@@ -0,0 +1,8 @@
+# Indentation is crucial in Python
+# IndentationError
+
+print (f"""
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+""")
---
+++ Python/Day-#1/3-Input.py
@@ -0,0 +1,7 @@
+# print("Hello " + input("what is your Name? "))
+# what is your Name? Nandisha
+# Hello Nandisha
+
+print(len(input("What is your Name? ")))
+# What is your Name? Nandisha
+# 8
