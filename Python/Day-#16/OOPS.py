f68b9d2ceba24eab749a161f9d28b304a9906cde
nkrishnappa <nandisha111@gmail.com>
2021-08-16 22:04:46 +0530

    #DAY16 #100DaysOfCode OOPS

---
+++ Python/Day-#16/OOPS.py
@@ -0,0 +1,18 @@
+'''
+
+Procedural and OOP
+
+How?
+    Objects - instance of a class
+    Classes - Blue prints
+
+Waiter -
+    what he has (attributes) : is holding plate = True, tables responsible = [4, 5, 6]
+    what is does (methods)- take order, take_payment
+
+object = Class
+manoj = Waiter() 
+
+Turtle Graphics
+
+'''
